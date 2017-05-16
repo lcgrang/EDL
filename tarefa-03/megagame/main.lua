9520d9f9a1225d52e14d25d464a9c45e6d8eced1
Luis Grangeiro <luiscarlos.fla@hotmail.com>
2017-05-16 01:42:48 -0300

    :art: altera visual do game e adiciona pontuação

--- tarefa-03/megagame/main.lua
+++ tarefa-03/megagame/main.lua
@@ -1,100 +1,92 @@
-local anim8 = require 'anim8'
-
-local animation, salto, imagem
 larguraDaTela = love.graphics.getWidth()
 alturaDaTela = love.graphics.getHeight()
-
-local posX = 100
-local posY = 100
-local direcao = true
-local saltou = false
-
 function love.load()
-    imagem = love.graphics.newImage("images/megaman.png")
-    fundo = love.graphics.newImage("images/fundo.jpg")
+    love.physics.setMeter(64)
+    world = love.physics.newWorld(0, 0, true)
 
-    local megamanCorrida = anim8.newGrid(35, 45, imagem:getWidth(), imagem:getHeight())
-    local megamanSalto = anim8.newGrid(49, 45, imagem:getWidth(), imagem:getHeight(), 120)
-
-    corrida = anim8.newAnimation(megamanCorrida('1-2', 3), 0.1)
-    salto = anim8.newAnimation(megamanSalto('1-2', 1), 0.1)
+    bloco = {}
+    bloco.body = love.physics.newBody(world, 10, 650/2, "dynamic")
+    bloco.shape = love.physics.newRectangleShape(50, 30)
+    bloco.fixture = love.physics.newFixture(bloco.body, bloco.shape)
+    velocidade = 5
 
     delayInimigo = 0.5
     tempoCriaInimigo = delayInimigo
-    inimigoImagem = love.graphics.newImage("images/bullet.png")
     inimigos = {}
 
-    estaVivo = true
+    pontos = 0
+    bonus = 0
+    perdas = 0
 end
 
 function love.update(dt)
-    megamanMoves(dt) 
+    world:update(dt) 
     criaInimigo(dt)
-    colisao()   
+    movimenta()
+    colisao()
 end
 
 function love.draw()
-    --love.graphics.setBackgroundColor(255, 255, 255)
-    love.graphics.draw(fundo, 0, 0, 0, 0.5, 0.6)
-    verificaDirecaoMegaman()
+love.graphics.setColor(240, 240, 240)
+    love.graphics.polygon("fill", bloco.body:getWorldPoints(bloco.shape:getPoints()))
 
     for i, inimigo in ipairs(inimigos) do
-        love.graphics.draw(inimigo.imagem, inimigo.posX, inimigo.posY, 0, 0.2, 0.2)
+        love.graphics.polygon("fill", inimigo.body:getWorldPoints(bloco.shape:getPoints()))
+    end
+    love.graphics.setColor(72, 240, 100)
+    love.graphics.print("Pontuação:", 5, 20)
+    love.graphics.print(pontos, 100, 20)
+
+    if perdas > 70 then
+        love.graphics.setColor(72, 240, 100)
+        love.graphics.print("Alerta", 5, 35)
+        love.graphics.print(pontos, 5, 50)
+    end
+
+    if perdas >= 100 then
+        inimigos = {}
+        pontos = 0
+        perdas = 0
+        bonus = 0
     end
 end
 
-function megamanMoves(dt)
-    if love.keyboard.isDown('left') then
-        saltou = false
-        if posX > 0 then
-            posX = posX - 150 * dt
-            if love.keyboard.isDown('a') then
-                posX = posX - 350 * dt
-            end
-        end
-        direcao = false
-        corrida:update(dt)
-    end
-
-    if love.keyboard.isDown('right') then
-        saltou = false
-        if posX < larguraDaTela then
-            posX = posX + 150 * dt
-            if love.keyboard.isDown('a') then
-                posX = posX + 350 * dt
-            end
-        end
-        direcao = true
-        corrida:update(dt)
-    end
-    if love.keyboard.isDown('up') then
-        saltou = true
-        if posY > 0 then
-            posY = posY - 150 * dt
-        end
-        salto:update(dt)
-    end
-    if love.keyboard.isDown('down') then
-        saltou = true
-        if posY < alturaDaTela then
-            posY = posY + 150 * dt            
-        end
-        salto:update(dt)
-    end
+function movimenta()
+if love.keyboard.isDown("a") then 
+    aceleracao = velocidade * 2
+else
+    aceleracao = velocidade
 end
 
-function verificaDirecaoMegaman()
-    if direcao then
-        if saltou then
-            salto:draw(imagem, posX, posY, 0,1,1,20,0)
+if love.keyboard.isDown("right") then 
+        if bloco.body:getX() < larguraDaTela then        
+            bloco.body:setX(bloco.body:getX() + aceleracao)
         else
-            corrida:draw(imagem, posX, posY, 0,1,1,20,0)
+            bloco.body:setX(bloco.body:getX())
         end
-    else 
-        if saltou then
-            salto:draw(imagem, posX, posY, 0,-1,1,20,0)
-        else
-            corrida:draw(imagem, posX, posY, 0,-1,1,20,0)
+    end  
+
+    if love.keyboard.isDown("left") then
+        if bloco.body:getX() > 0  then         
+            bloco.body:setX(bloco.body:getX() - aceleracao)
+        else 
+            bloco.body:setX(bloco.body:getX())
+        end
+    end
+
+    if love.keyboard.isDown("up") then
+        if bloco.body:getY() > 0  then         
+            bloco.body:setY(bloco.body:getY() - aceleracao)
+        else 
+            bloco.body:setY(bloco.body:getY())
+        end
+    end
+
+    if love.keyboard.isDown("down") then
+        if bloco.body:getY() < alturaDaTela  then         
+            bloco.body:setY(bloco.body:getY() + aceleracao)
+        else 
+            bloco.body:setY(bloco.body:getY())
         end
     end
 end
@@ -104,26 +96,37 @@ function criaInimigo(dt)
     if tempoCriaInimigo < 0 then
         tempoCriaInimigo = delayInimigo
         nRandom = math.random(10, love.graphics.getHeight())
-        novoInimigo = {posX = love.graphics.getWidth() - 10, posY = nRandom, imagem = inimigoImagem}
-        table.insert(inimigos, novoInimigo)
+        inimigo = {}
+        inimigo.body = love.physics.newBody(world, love.graphics.getWidth() - 10, nRandom, "dynamic")
+        inimigo.shape = love.physics.newRectangleShape(50, 30)  
+        inimigo.fixture = love.physics.newFixture(bloco.body, bloco.shape)
+        --novoInimigo = {posX = love.graphics.getWidth() - 10, posY = nRandom, imagem = inimigoImagem}
+        table.insert(inimigos, inimigo)
     end
 
     for i, inimigo in ipairs(inimigos) do
-        inimigo.posX = inimigo.posX - (100*dt)
-        if inimigo.posX < 0 then
+        inimigo.body:setX(inimigo.body:getX() - (170*dt))
+        if inimigo.body:getX() < 0 then
             table.remove(inimigos, i)
+            perdas = perdas + 1
         end
     end
+end
+
+function checaColisao(x1, y1, w1, h1, x2, y2, w2, h2)
+    return x1 < x2 + w2 and x2 < x1 + w1 and y1 < y2 + h2 and y2 < y1 + w1
 end
 
 function colisao()
     for i, inimigo in ipairs(inimigos) do
-        if checaColisao(inimigo.posX, inimigo.posY, posX, posY) then
+        if checaColisao(inimigo.body:getX(), inimigo.body:getY(), 50, 30, bloco.body:getX(), bloco.body:getY(), 50, 30) then
             table.remove(inimigos, i)
+            pontos = pontos + 1
+            bonus = bonus + 1
+            if bonus >= 100 then
+                bonus = 0
+                perdas = 0
+            end
         end
     end
-end
-
-function checaColisao(x1, y1, x2, y2)
-    return x1 == x2
 end
